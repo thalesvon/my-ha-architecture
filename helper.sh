36b202016dde8a1364def63a6dffcf08ed28fea7
Thales von Sperling <sperlt@amazon.com>
2020-01-12 19:45:32 +0000

    adding helper script to create/update stacks

---
+++ helper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PROJECT='my-ha-architecture'
+
+aws s3 ls s3://${PROJECT} &> /dev/null
+if [ $? -ne 0 ];then
+    echo 'Bucket does not exist, creating one...'
+    aws s3 mb s3://${PROJECT}
+fi
+
+aws s3 sync . s3://${PROJECT} --exclude helper.sh --exclude ".git/*"
+
+aws cloudformation describe-stacks --stack-name ${PROJECT} &> /dev/null
+if [ $? -ne 0 ];then
+    echo 'Stack does not exist, create will be executed...'
+    aws cloudformation create-stack --stack-name ${PROJECT} \
+                                    --capabilities CAPABILITY_IAM \
+                                    --template-body file://main.yml \
+                                    --parameters ParameterKey=TemplatesBucket,ParameterValue=${PROJECT} ParameterKey=ProjectName,ParameterValue=${PROJECT}
+else
+    echo 'Stack exists, update will be executed...'
+    aws cloudformation update-stack --stack-name ${PROJECT} \
+                                    --capabilities CAPABILITY_IAM \
+                                    --template-body file://main.yml \
+                                    --parameters ParameterKey=TemplatesBucket,ParameterValue=${PROJECT} ParameterKey=ProjectName,ParameterValue=${PROJECT}
+                                    
+fi
